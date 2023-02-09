0ac19171b7e77beb21b054fd498cf67cc52919f4
vikramkumarphython <123940240+vikramkumarphython@users.noreply.github.com>
2023-02-09 19:19:41 +0530

    Add files via upload

---
+++ DAY 2 C--P1.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("\n\n\t\tStudy- Best place to learn\n\n\n");
+	int i = 0; 
+	for(i = 0; i < 10; i++)
+    {
+        printf("i = %d\n", i);
+
+    }
+	 return 0;
+}  
---
+++ DAY2 C---P3.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("\n\n\t\t- Best place to learn\n\n\n");
+
+   
+    int i = 10;   
+    do // 
+    {
+        printf("i = %d\n",i);
+        i = i-1;    
+    }
+    while(i > 0);
+     
+    printf("\n\The value of i after exiting the loop is %d\n\n", i);
+    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+    return 0;
+}
